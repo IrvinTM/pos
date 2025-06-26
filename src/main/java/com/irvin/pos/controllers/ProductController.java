3b23a0cadb64ca9db90e76a1289f486217ecc683
IrvinTM <irvintm@gh.com>
2025-06-26 15:48:33 +0000

    CORS wildcard

--- src/main/java/com/irvin/pos/controllers/ProductController.java
+++ src/main/java/com/irvin/pos/controllers/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public CustomPageDTO<ProductDTO> getAllProducts() {
         return productService.getAllProducts();
