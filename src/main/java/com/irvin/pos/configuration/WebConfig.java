c446e2f84bb961aeda3592d7df5fcfad0c5a3ec3
IrvinTM <irvintm@gh.com>
2025-06-26 16:48:21 +0000

    cors test 2

--- src/main/java/com/irvin/pos/configuration/WebConfig.java
+++ src/main/java/com/irvin/pos/configuration/WebConfig.java
@@ -1,5 +1,4 @@
 package com.irvin.pos.configuration;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry){
             registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://5173-cs-9ec2fbb7-98c4-448b-ad44-567877072fc1.cs-us-west1-vwey.cloudshell.dev/?authuser=0")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
