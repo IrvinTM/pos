151a8bd1b5148a2e4c7c7268717eed18fcb2ad89
IrvinTM <irvintm@gh.com>
2025-06-27 15:40:23 +0000

    allow all origins

--- src/main/java/com/irvin/pos/configuration/WebConfig.java
+++ src/main/java/com/irvin/pos/configuration/WebConfig.java
@@ -9,7 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry){
             registry.addMapping("/**")
-                .allowedOriginPatterns("https://5173-cs-9ec2fbb7-98c4-448b-ad44-567877072fc1.cs-us-west1-vwey.cloudshell.dev/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
