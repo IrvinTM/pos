725e8c7fe4c216af19ef1924ee21b49e8a8ee5f3
IrvinTM <irvintm@gh.com>
2025-06-27 16:00:23 +0000

    allow all CORS

--- src/main/java/com/irvin/pos/configuration/WebConfig.java
+++ src/main/java/com/irvin/pos/configuration/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry){
             registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://imerlos-dev.web.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
         }
