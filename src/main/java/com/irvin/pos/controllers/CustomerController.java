1676179efbe6d455fe8d180cfd5cac46b6426ac8
IrvinTM <irvintm23@gmail.com>
2025-07-24 22:56:34 -0600

    added sale endpoints

---
+++ src/main/java/com/irvin/pos/controllers/CustomerController.java
@@ -0,0 +1,31 @@
+package com.irvin.pos.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.irvin.pos.dtos.CustomPageDTO;
+import com.irvin.pos.entities.Customer;
+import com.irvin.pos.services.CustomerService;
+
+@RestController()
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping()
+    public ResponseEntity<CustomPageDTO<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    @PostMapping("/create")
+    public Customer creaCustomer(@RequestBody Customer customer) throws Exception {
+        return customerService.createCustomer(customer);
+    }
+}
--- src/main/java/com/irvin/pos/services/CustomerService.java
+++ src/main/java/com/irvin/pos/services/CustomerService.java
@@ -4,16 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import com.irvin.pos.dtos.CustomPageDTO;
 import com.irvin.pos.entities.Customer;
 import com.irvin.pos.exceptions.PropertyAlreadyExistException;
 import com.irvin.pos.repositories.CustomerRepository;
+import com.irvin.pos.utils.CustomPage;
 
 @Service
 public class CustomerService {
 
     @Autowired
     CustomerRepository customerRepository;
-    
+
     public Customer createCustomer(Customer customer) throws PropertyAlreadyExistException {
         if (customerRepository.getByIdentification(customer.getIdentification()) != null
 
@@ -26,17 +29,22 @@ public class CustomerService {
         }
         return customerRepository.save(customer);
     }
-    
-    public Customer updateCustomer(Customer customer){
+
+    public Customer updateCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
 
-    public void deleteCustomer(long id){
+    public void deleteCustomer(long id) {
         customerRepository.deleteById(id);
     }
 
-    public Page<Customer> getAllCustomers(){
-        Page<Customer> peoplePage =customerRepository.findAll(PageRequest.of(0, 10));
-        return peoplePage;
+    public CustomPageDTO<Customer> getAllCustomers() {
+        Page<Customer> peoplePage = customerRepository.findAll(PageRequest.of(0, 10));
+        CustomPageDTO<Customer> page = new CustomPageDTO<>();
+        CustomPage p = new CustomPage(peoplePage.getTotalElements(), peoplePage.getTotalPages(), peoplePage.getNumber(),
+                peoplePage.getSize());
+        page.setCustomPage(p);
+        page.setContent(peoplePage.getContent());
+        return page;
     }
 }
